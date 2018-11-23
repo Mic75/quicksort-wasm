ee0fb9a50819bc6a0bdce246943df9844802fd2f
Mathieu Benard <mathieub@unity3d.com>
2018-11-23 18:51:18 -0500

    Get wasm quicksort working

--- src/build.bat
+++ src/build.bat
@@ -1,1 +1,1 @@
-emcc --emrun quicksort.cpp -o quicksort-wasm.js -s EXPORTED_FUNCTIONS="['_sort']" -s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap']" -s EXPORT_ES6=1 -s MODULARIZE=1
+emcc quicksort.cpp -o quicksort-wasm.js -s EXPORTED_FUNCTIONS="['_sort']" -s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall']" -s EXPORT_ES6=1 -s MODULARIZE=1
