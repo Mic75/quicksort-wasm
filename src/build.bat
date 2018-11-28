596adeb376b79dff39f1f4da028afbd2f225b5d6
Mic75 <mat.benard@gmail.com>
2018-11-27 22:26:54 -0500

    Works on array copies instead of reusing same array

--- src/build.bat
+++ src/build.bat
@@ -1,1 +1,1 @@
-emcc quicksort.cpp -o quicksort-wasm.js -s EXPORTED_FUNCTIONS="['_quicksort']" -s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall']" -s EXPORT_ES6=1 -s MODULARIZE=1
+emcc -O3 quicksort.cpp -o quicksort-wasm.js -s EXPORTED_FUNCTIONS="['_quicksort', '_malloc', '_free']" -s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall']" -s EXPORT_ES6=1 -s MODULARIZE=1
